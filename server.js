102489f41107fad5a1230549df5bed3cc8dc0e2b
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-03-24 22:42:11 -0600

    fixing the undefined variable inside the try/catch block that was added

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.get('/home', async (req,res) => {
         res.status(200).json(rows)
     }
     catch(err) {
-        res.status(500).json({"message": `Could not retrieve quotes. ${err} ${id}`})
+        res.status(500).json({"message": `Could not retrieve quotes.`})
     }
 })
 
